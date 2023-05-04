491f48eca6d6a8632ee823125d018117ea48c611
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2023-05-03 23:27:35 -0400

    fix/misc: Use up-to-date gruvbox.nvim config.
    Change highlight groups to get rid of gray bg for SignColumn, lsp diagnostics, and gitsigns.

--- home/config/nvim/lua/colors/gruvbox.lua
+++ home/config/nvim/lua/colors/gruvbox.lua
@@ -1,17 +1,17 @@
 -- setup must be called before loading the colorscheme
 -- Default options:
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = true, -- will make italic comments and special strings
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  contrast = "hard", -- can be "hard" or "soft"
-  overrides = {},
+    overrides = {
+        SignColumn = { link = "Normal" },
+        LineNr = { fg = "LightGrey" },
+        GruvboxGreenSign = { bg = "" },
+        GruvboxOrangeSign = { bg = "" },
+        GruvboxPurpleSign = { bg = "" },
+        GruvboxYellowSign = { bg = "" },
+        GruvboxRedSign = { bg = "" },
+        GruvboxBlueSign = { bg = "" },
+        GruvboxAquaSign = { bg = "" },
+    }
 })
 
 vim.opt.background = "dark" -- or "light" for light mode
