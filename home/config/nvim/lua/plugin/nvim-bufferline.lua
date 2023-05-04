2949d554775d1e8ba3ce5ce2c13fcd846a82ebbf
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2023-05-03 23:36:17 -0400

    misc: Change keymap and remove NvimTree integration.

--- home/config/nvim/lua/plugin/nvim-bufferline.lua
+++ home/config/nvim/lua/plugin/nvim-bufferline.lua
@@ -1,14 +1,6 @@
 -- https://github.com/akinsho/nvim-bufferline.lua
 require("bufferline").setup {
     options = {
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text = "File Explorer",
-                highlight = "Directory",
-                text_align = "left"
-            }
-        },
         diagnostics = "nvim_lsp"
     }
 }
@@ -17,6 +9,5 @@ require("bufferline").setup {
 vim.keymap.set("n", "gb", ":BufferLinePick<CR>", { silent = true }) -- Choose Buffer
 vim.keymap.set("n", "<leader>n", ":BufferLineCycleNext<CR>", { silent = true }) -- Next Buffer
 vim.keymap.set("n", "<leader>p", ":BufferLineCyclePrev<CR>", { silent = true }) -- Prev Buffer
-vim.keymap.set("n", "<M-n>", ":BufferLineMoveNext<CR>", { silent = true }) -- Next Buffer
-vim.keymap.set("n", "<M-p>", ":BufferLineMovePrev<CR>", { silent = true }) -- Prev Buffer
-vim.keymap.set("n", "<leader>bp", ":BufferLineTogglePin<CR>", { silent = true }) -- Prev Buffer
+vim.keymap.set("n", "<leader>bn", ":BufferLineMoveNext<CR>", { silent = true }) -- Move Next Buffer
+vim.keymap.set("n", "<leader>bp", ":BufferLineMovePrev<CR>", { silent = true }) -- Move Prev Buffer
