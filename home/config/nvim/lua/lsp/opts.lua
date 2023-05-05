2a6a3520745936d099f6a208b5f5812e91192fa2
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2023-05-04 22:01:47 -0400

    feat/misc: Add loclist and formatting.
    Change space to leader. Leader key is space.

--- home/config/nvim/lua/lsp/opts.lua
+++ home/config/nvim/lua/lsp/opts.lua
@@ -19,19 +19,21 @@ M = {
         set_keymap('n', 'K', vim.lsp.buf.hover, bufopts)
         set_keymap('i', '<C-s>', vim.lsp.buf.signature_help, bufopts)
         set_keymap('n', 'gT', vim.lsp.buf.type_definition, bufopts)
-        set_keymap('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-        set_keymap('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        set_keymap('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+        set_keymap('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-        set_keymap('n', '<space>e', vim.diagnostic.open_float, opts)
+        set_keymap('n', '<leader>e', vim.diagnostic.open_float, opts)
         set_keymap('n', '<leader>[', vim.diagnostic.goto_prev, opts)
         set_keymap('n', '<leader>]', vim.diagnostic.goto_next, opts)
+        set_keymap('n', '<leader>q', vim.diagnostic.setloclist, opts)
+        set_keymap("n", "<space>f=", function()
+            vim.lsp.buf.format { async = true }
+        end, bufopts)
 
         -- Unused
         -- set_keymap('n', 'gr', telescope.builtin.lsp_references(), opts) -- Implemented w/ Telescope in keymaps.lua
         -- set_keymap('n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts) -- Implemented w/ Telescope in keymaps.lua
-        -- set_keymap('n', '<space>q', vim.diagnostic.set_loclist, opts)
-        -- set_keymap("n", "<space>f", vim.lsp.buf.formatting, bufopts)
         -- set_keymap('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
         -- set_keymap('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
         -- set_keymap('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
