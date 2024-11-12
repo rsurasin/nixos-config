e46c7af9551eb5f1100e068e294f171ac699fcc0
Rahul Surasinghe <rsurasin@gmail.com>
2024-11-11 20:12:30 -0500

    fix/refactor(Neovim): Make tsx and jsx recognizable -- needed their own .lua file. Move all autocmds to ../after/plugin/autocmds.lua

--- home/config/nvim/after/plugin/autocmds.lua
+++ home/config/nvim/after/plugin/autocmds.lua
@@ -11,8 +11,38 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- Remove trainling whitespace on all files
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-      vim.cmd([[%s/\s\+$//e]])
-    end,
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
+
+-- For TS/JS, check if Eslint LSP is attached, run `:EslintFixAll`
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx" },
+  callback = function()
+    if vim.fn.exists(":EslintFixAll") > 0 then
+      vim.cmd("EslintFixAll")
+    end
+  end,
+})
+
+-- For Python, check if `black` formatter is available and run it
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.py",
+  callback = function()
+    if vim.fn.executable("black") == 1 then
+      vim.fn.jobstart({ "black", vim.api.nvim_buf_get_name(0) })
+    end
+  end,
+})
+
+-- For Go, check if `go fmt` is available and run it
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.go",
+  callback = function()
+    if vim.fn.executable("go") == 1 then
+      vim.fn.jobstart({ "go", "fmt", vim.api.nvim_buf_get_name(0) })
+    end
+  end,
 })
