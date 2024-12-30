0cb57eb85ff40be4b8376e60ccfe5d5c484cc7af
Rahul Surasinghe <rsurasin@gmail.com>
2024-12-30 10:14:20 -0500

    feat: Allow TCP ports for React-Native and Expo development. Need to open more ports for development later
    Remove whitespace

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,7 +18,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
+
   # fish autocomplete for system packages
   environment.pathsToLink = [
     "/share/fish"
@@ -134,7 +134,7 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-    
+
     wireplumber.enable = true;
   };
 
@@ -206,7 +206,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [8081 5173];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
